396dd12e447b42b7aec11ec2cd1750258c5926b5
pavel <a123@MacBook-Pro-123.local>
2023-06-19 14:53:36 +0300

    awiudhawiufeaiuhglkrwgughfv

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ stack = []
 a = input()
 bad = False
 for i in a:
-    if i == "(" or i == "[":
+    if i == "(" or i == "[" or i == "{":
         stack.append(i)
     else:
         if len(stack) == 0:
             bad = True
             break
-        if stack[-1] == "(" and i != "]" or stack[-1] == "[" and i != ")":
+        if stack[-1] == "(" and i == ")" or stack[-1] == "[" and i == "]" or stack[-1] == "{" and i == "}":
             stack.pop()
         else:
             bad = True
             break
+# jfdnasfjasfwaeiuhfiesrhgfdhrfgiuhdfbjkhfdhrofigjheriokgoudfhjgildrhjgiuergjo
 
 if len(stack) != 0:
     bad = True
 
 if bad:
-    print("Неправильно")
+    print("no")
 else:
-    print("Правильно")
+    print("yes")
