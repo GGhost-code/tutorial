ff9e31838b00ccd0300aa66c97085ecd2669979f
a123 <pavelkot2209@gmail.com>
2023-06-26 13:51:49 +0300

    good

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 stack = []
-a = input()
-bad = False
-for i in a:
-    if i == "(" or i == "[" or i == "{":
-        stack.append(i)
-    else:
-        if len(stack) == 0:
-            bad = True
+stack2 = []
+a = input() + " "
+c = 0
+num = ""
+while True:
+    while True:
+        i = a[c]
+        c += 1
+        if (ord('1') > ord(i) or ord(i) > ord('9')) and c != 1:
             break
-        if stack[-1] == "(" and i == ")" or stack[-1] == "[" and i == "]" or stack[-1] == "{" and i == "}":
-            stack.pop()
-        else:
-            bad = True
-            break
-# jfdnasfjasfwaeiuhfiesrhgfdhrfgiuhdfbjkhfdhrofigjheriokgoudfhjgildrhjgiuergjo
+        num += i
+    stack.append(int(num))
+    if i == " ":
+        break
+    stack2.append(i)
+    num = ""
+c = 0
+for i in range(len(stack2)):
+    if stack2[i] == "*":
+        a1 = stack.pop(i - c)
+        a2 = stack.pop(i - c)
+        stack.insert(i - c, a1 * a2)
+        c += 1
+    elif stack2[i] == "/":
+        a1 = stack.pop(i - c)
+        a2 = stack.pop(i - c)
+        stack.insert(i - c, a1 // a2)
+        c += 1
+c = 0
+for i in range(len(stack2)):
+    if stack2[i] == "+":
+        a1 = stack.pop(0)
+        a2 = stack.pop(0)
+        stack.insert(0, a1 + a2)
+    elif stack2[i] == "-":
+        a1 = stack.pop(0)
+        a2 = stack.pop(0)
+        stack.insert(0, a1 - a2)
 
-if len(stack) != 0:
-    bad = True
-
-if bad:
-    print("no")
-else:
-    print("yes")
+print(stack[0])
